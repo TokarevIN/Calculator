acd8cd707f2b1af366c288fa35922bc441026351
Ilya_Tokarev <sbstokarev@yandex.ru>
2024-06-26 22:09:44 +0700

    Initial commit

--- src/arabian/Converter.java
+++ src/arabian/Converter.java
@@ -31,43 +31,51 @@ public class Converter {
 
     }
 
-
-    public boolean isRoman(String number){
-        return romanKeyMap.containsKey(number.charAt(0));
-    }
-
-    //15
-    public String intToRoman(int number) {
-        String roman = "";
-        int arabianKey;
-        do {
-            arabianKey = arabianKeyMap.floorKey(number);
-            roman += arabianKeyMap.get(arabianKey);
-            number -= arabianKey;
-        } while (number != 0);
-        return roman;
-
-
-    }
-    //XV
-    public int romanToInt(String s) {
-        int end = s.length() - 1;
-        char[] arr = s.toCharArray();
-        int arabian;
-        int result = romanKeyMap.get(arr[end]);
-        for (int i = end - 1; i >= 0; i--) {
-            arabian = romanKeyMap.get(arr[i]);
-
-            if (arabian < romanKeyMap.get(arr[i + 1])) {
-                result -= arabian;
-            } else {
-                result += arabian;
-            }
-
-
+    public boolean isRoman(String number) {
+        if (number == null || number.isEmpty()) {
+            return false;
         }
-        return result;
 
+        // Регулярное выражение для стандартного и нестандартного формата римских чисел
+        String romanPattern = "^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(III|IV|V?I{0,3})$";
+        return number.matches(romanPattern);
+    }
+
+    public String intToRoman(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("Число должно быть положительным");
+        }
+
+        StringBuilder roman = new StringBuilder();
+
+        while (number > 0) {
+            Integer key = arabianKeyMap.floorKey(number);
+            String romanNumeral = arabianKeyMap.get(key);
+            roman.append(romanNumeral);
+            number -= key;
+        }
+
+        return roman.toString();
+    }
+
+    public int romanToInt(String s) {
+        if (!isRoman(s)) {
+            throw new IllegalArgumentException("Неверное римское число");
+        }
+
+        int result = 0;
+        int i = 0;
+
+        while (i < s.length()) {
+            if (i + 1 < s.length() && romanKeyMap.get(s.charAt(i)) < romanKeyMap.get(s.charAt(i + 1))) {
+                result += romanKeyMap.get(s.charAt(i + 1)) - romanKeyMap.get(s.charAt(i));
+                i += 2;
+            } else {
+                result += romanKeyMap.get(s.charAt(i));
+                i++;
+            }
+        }
+
+        return result;
     }
 }
-
